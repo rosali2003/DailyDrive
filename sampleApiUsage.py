4e222d75e5981ba5430442f487374f3ab896654e
Hamza Khattab <hamzakhattab7@gmail.com>
2024-09-28 03:10:47 -0400

    Fixed API token being used in public project

--- sampleApiUsage.py
+++ sampleApiUsage.py
@@ -3,11 +3,7 @@ from openai import OpenAI
 # gets API Key from environment variable OPENAI_API_KEY
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-<<<<<<< HEAD
   api_key="YOUR_KEY",  # replace with your API key
-=======
-  api_key="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",
->>>>>>> 906fd29f2a6635b83e1351f0599800044b0f5d05
 )
 
 completion = client.chat.completions.create(
